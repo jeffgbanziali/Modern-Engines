ebfc0d3327e96a0e8eb39446ed37c323eefd0319
jeffgbanziali <91453731+jeffgbanziali@users.noreply.github.com>
2022-05-08 10:21:18 +0200

    commit again

--- components/screen/HomeScreen/Accueil.js
+++ components/screen/HomeScreen/Accueil.js
@@ -1,9 +1,12 @@
-import { View, Text, ScrollView } from "react-native";
+import { View, Text, ScrollView, Video, StyleSheet } from "react-native";
 import React from "react";
 import SearchBar from "../../SearchBar/SearchBar";
 import Button from "../../Button/Button";
 
 const Accueil = () => {
+  const video = React.useRef(null);
+  const [status, setStatus] = React.useState(null);
+
   return (
     <ScrollView ScrollView style={{ backgroundColor: "#2D55A2" }}>
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
@@ -18,6 +21,7 @@ const Accueil = () => {
         <SearchBar />
         <Button text="Rechercher" />
       </View>
+
       <View style={{ marginTop: 2, alignItems: "center" }}>
         <Text style={{ fontSize: 16, fontWeight: "bold", marginTop: "50%" }}>
           Essentiels de notre application
@@ -27,4 +31,16 @@ const Accueil = () => {
   );
 };
 
+const styles = StyleSheet.create({
+  Video: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  video: {
+    flex: 1,
+    alignSelf: "stretch",
+  },
+});
 export default Accueil;
